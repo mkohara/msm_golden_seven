86b939857f96da965be7aa22b73a58fa5841e5a0
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-02-07 21:21:14 -0600

    Have the two directors and movies working
    working on delete/edit now

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -8,4 +8,23 @@ class DatabaseController < ApplicationController
   def directors
     @list_of_directors = Director.all
   end
+
+  def show_movie
+    @movie = Movie.find_by({ :id => params[:id]})
+    @movie_title = @movie.title
+    @movie_year = @movie.year
+    @movie_duration =@movie.duration
+    @movie_description = @movie.description
+    @movie_image = @movie.image_url
+  end
+
+  def show_director
+    @director= Director.find_by({:id =>params[:id]})
+  end
+
+  def destroy_director
+    director= Director.find_by({:id =>params[:id]})
+    director.destroy
+    redirect_to("/directors")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   #get("/", {:controller=>"database", :action=>"home"})
 
   get("/", {:controller => "database", :action=>"index"})
+  get("/movies", {:controller => "database", :action=>"index"})
   get("/directors", {:controller => "database", :action=>"directors"})
+
+  get("/directors/:id", {:controller=> "database", :action => "show_director"})
+  get("/movies/:id", {:controller=> "database", :action => "show_movie"})
+
+  get("/movies/delete_movie/:id", {:controller=> "database", :action => "destroy_movie"})
+  get("/directors/delete_director/:id", {:controller=> "database", :action => "destroy_director"})
 end
