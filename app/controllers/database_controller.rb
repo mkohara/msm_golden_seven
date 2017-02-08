80f59556858a38c7cc98de29597b38dd9dba4821
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-02-07 22:11:35 -0600

    still working

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -27,4 +27,39 @@ class DatabaseController < ApplicationController
     director.destroy
     redirect_to("/directors")
   end
+
+  def new_form_directors
+    @director = Director.find_by({:id => params[:id]})
+
+  end
+
+  def create_row_directors
+    n = Director.new
+    n.name=params[:name]
+    n.bio=params[:bio]
+    n.dob=params[:dob]
+    n.image_url=params[:image_url]
+    n.save
+
+  end
+
+  def edit_form_director
+    @director = Director.find_by({:id => params[:id]})
+    n = Director.new
+    n.name=params[:name]
+    n.bio=params[:bio]
+    n.dob=params[:dob]
+    n.image_url=params[:image_url]
+  end
+
+  def update_row_director
+    u = Director.find_by({:id => params[:id]})
+    @director = Director.find_by({:id => params[:id]})
+    u.image_url = params[:image_url]
+    u.name = params[:name]
+    u.bio = params[:bio]
+    u.dob = params[:dob]
+    u.save
+    redirect_to("/directors/#{@director.id}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,20 @@ Rails.application.routes.draw do
   get("/movies", {:controller => "database", :action=>"index"})
   get("/directors", {:controller => "database", :action=>"directors"})
 
+
+  get("/directors/:id/edit_form_director", {:controller=>"database", :action=> "edit_form_director"})
+  get("/directors/:id/update_row_director", {:controller => "database", :action =>"update_row_director"})
+
   get("/directors/:id", {:controller=> "database", :action => "show_director"})
   get("/movies/:id", {:controller=> "database", :action => "show_movie"})
-
+#destroy
   get("/movies/delete_movie/:id", {:controller=> "database", :action => "destroy_movie"})
   get("/directors/delete_director/:id", {:controller=> "database", :action => "destroy_director"})
+
+#update
+  get("/directors/new_form_directors", {:controller=> "database", :action => "new_form_directors"})
+  
+  get("/directors/create_row_directors", {:controller=> "database", :action => "create_row_directors"})
+  get("/directors/:id/edit_form_director", {:controller=>"database", :action=> "edit_form_director"})
+  get("/directors/:id/update_row_director", {:controller => "database", :action =>"update_row_director"})
 end
